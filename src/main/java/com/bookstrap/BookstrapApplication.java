bb88182fa51fc8f7b277c44bc03c145bd5d02bd9
leoacheng <leoacheng@gmail.com>
2023-02-06 17:54:40 +0800

    更改名稱
    將Booktopia 全改成Bookstrap

--- src/main/java/com/bookstrap/BookstrapApplication.java
+++ src/main/java/com/bookstrap/BookstrapApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BooktopiaApplication {
+public class BookstrapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BooktopiaApplication.class, args);
+		SpringApplication.run(BookstrapApplication.class, args);
 	}
 
 }
