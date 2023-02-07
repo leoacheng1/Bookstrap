d681a1b2abfa5c6a5bc6b8821eb1513b82694528
Harrycheng <harrycheng1992@gmail.com>
2023-02-07 10:41:09 +0800

    Members update
    1. add Members & MemberDetails
    2. add MemberRepository
    3. add  MemberService

---
+++ src/main/java/com/bookstrap/model/MemberDetails.java
@@ -0,0 +1,153 @@
+package com.bookstrap.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+
+@Entity
+@Table(name = "MemberDetails")
+public class MemberDetails {
+
+	
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "member_id")
+private Integer memberId;
+
+@Column(name = "member_name")
+private String memberName;
+
+@Lob
+@Column(name = "photo")
+private byte memberPhoto;
+
+@Column(name = "sex")
+private Integer memberSex;
+
+@Column(name = "email")
+private String memberEmail;
+
+@Column(name = "cellphone")
+private String memberPhone;
+
+@Column(name = "address")
+private String memberAddress;
+
+@Column(name = "birthday")
+@JsonFormat(pattern = "yyyy/MM/dd", timezone = "GMT+8")
+private Date memberBirthday;
+
+
+
+public MemberDetails() {
+}
+
+
+
+public Integer getMemberId() {
+	return memberId;
+}
+
+
+
+public void setMemberId(Integer memberId) {
+	this.memberId = memberId;
+}
+
+
+
+public String getMemberName() {
+	return memberName;
+}
+
+
+
+public void setMemberName(String memberName) {
+	this.memberName = memberName;
+}
+
+
+
+public byte getMemberPhoto() {
+	return memberPhoto;
+}
+
+
+
+public void setMemberPhoto(byte memberPhoto) {
+	this.memberPhoto = memberPhoto;
+}
+
+
+
+public Integer getMemberSex() {
+	return memberSex;
+}
+
+
+
+public void setMemberSex(Integer memberSex) {
+	this.memberSex = memberSex;
+}
+
+
+
+public String getMemberEmail() {
+	return memberEmail;
+}
+
+
+
+public void setMemberEmail(String memberEmail) {
+	this.memberEmail = memberEmail;
+}
+
+
+
+public String getMemberPhone() {
+	return memberPhone;
+}
+
+
+
+public void setMemberPhone(String memberPhone) {
+	this.memberPhone = memberPhone;
+}
+
+
+
+public String getMemberAddress() {
+	return memberAddress;
+}
+
+
+
+public void setMemberAddress(String memberAddress) {
+	this.memberAddress = memberAddress;
+}
+
+
+
+public Date getMemberBirthday() {
+	return memberBirthday;
+}
+
+
+
+public void setMemberBirthday(Date memberBirthday) {
+	this.memberBirthday = memberBirthday;
+}
+	
+	
+	
+	
+}
