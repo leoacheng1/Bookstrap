c704ec3fb77f78d12a2ac1db70fa10dbf642f081
Allen <Test@gmail.com>
2023-02-07 17:18:37 +0800

    更改shop
    shop ajax insert selectall

--- src/main/java/com/bookstrap/controller/ShopsController.java
+++ src/main/java/com/bookstrap/controller/ShopsController.java
@@ -1,19 +1,36 @@
 package com.bookstrap.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bookstrap.model.Shops;
 import com.bookstrap.service.ShopsService;
 
 @Controller
 public class ShopsController {
-
+	
+	@Autowired
 	private ShopsService shService;
 	
 	
-	@GetMapping("/shops/insert")
+	@GetMapping("/shops/home")
 	public String uploadPage() {
-		return "shop/shopInsert";
+		return "shop/shophome";
 	}
-	
+    @ResponseBody
+	@PostMapping("/shops/add")
+	public Shops insert(@RequestBody Shops sh) {
+		return shService.insertShop(sh);
+	}
+    @ResponseBody
+    @GetMapping("/shops/all")
+	public List<Shops> findAllCustomer() {
+		return shService.findAllShop();
+	}
 }
