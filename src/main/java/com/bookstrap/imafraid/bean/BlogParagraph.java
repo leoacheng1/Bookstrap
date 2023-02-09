1289b52b4ffb33310a03af2e30e83a5a4b450459
R.G <shimatsuki26@gmail.com>
2023-02-09 19:50:59 +0800

    可能是給BLOG用的Beans

---
+++ src/main/java/com/bookstrap/imafraid/bean/BlogParagraph.java
@@ -0,0 +1,12 @@
+package com.bookstrap.imafraid.bean;
+
+public class BlogParagraph {
+	private Integer paragraphId;
+	private String paragraphData01;
+	private String paragraphData02;
+	private String paragraphData03;
+	private String paragraphAuther;
+	private String paragraphCatagory;
+
+
+}
---
+++ src/main/java/com/bookstrap/imafraid/bean/BlogThings.java
@@ -0,0 +1,9 @@
+package com.bookstrap.imafraid.bean;
+
+
+public class BlogThings {
+	private Integer blogPhotoId;
+	private String blogPhotoName;
+	private byte blogPhoto;
+	private 
+}
