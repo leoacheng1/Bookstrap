ef7f40a0c4ae0dc8624213ba282c306b00d6b601
Harrycheng1992 <66951332+Harrycheng1992@users.noreply.github.com>
2023-02-09 09:34:30 +0800

    Revert "Revert "Merge remote-tracking branch 'origin/eddie' into harry""
    This reverts commit c240a6bac1fbbe4da8b35691c9c8cb01fb0e5476.

--- src/main/java/com/bookstrap/controller/ShoppingCartController.java
+++ src/main/java/com/bookstrap/controller/ShoppingCartController.java
@@ -1,13 +1,5 @@
 package com.bookstrap.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+public class ShoppingcartController {
 
-@Controller
-public class ShoppingCartController {
-
-	@GetMapping("/shoppingcart/")
-	public String getAllBooksByMemberId() {
-		return null;
-	}
 }
