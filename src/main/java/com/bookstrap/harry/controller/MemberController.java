a2408848ca4477274885823c99cafa6d1e1fd505
Harrycheng1992 <66951332+Harrycheng1992@users.noreply.github.com>
2023-02-10 13:56:16 +0800

    Update MemberController.java

--- src/main/java/com/bookstrap/harry/controller/MemberController.java
+++ src/main/java/com/bookstrap/harry/controller/MemberController.java
@@ -34,47 +34,47 @@ public class MemberController {
 	}
 	
 	//Registration
-//	@PostMapping("/member/post")
-//	public String memberSignUp(@RequestParam("memberName") String memberName, 
-//			@RequestParam("memberEmail") String memberAccount, 
-//			@RequestParam("memberEmail") String memberEmail, 
-//			@RequestParam("memberPassword") String memberPassword, 
-//			@RequestParam("membervalid") Integer memberValid,
-//			@RequestParam("memberLevel") Integer memberLevel,
-//			@RequestParam("memberSex") Integer memberSex,
-//			@RequestParam("memberBirthday") Date memberBirthday,
-//			@RequestParam("memberPhone") String memberPhone, 
-//			@RequestParam("memberPhone") String memberCellPhone,
-//			@RequestParam("memberAddress") String memberAddress) {
-//		Members member = new Members();
-//		MemberDetails memberDetail = new MemberDetails();
-//		memberDetail.setMemberName(memberName);
-//		member.setMemberAccount(memberAccount);
-//		memberDetail.setMemberEmail(memberEmail);
-//		member.setMemberPassword(memberPassword);
-//		member.setMemberValid(memberValid);
-//		member.setMemberLevel(memberLevel);
-//		memberDetail.setMemberSex(memberSex);
-//		memberDetail.setMemberBirthday(memberBirthday);
-//		memberDetail.setMemberPhone(memberPhone);
-//		memberDetail.setMemberAddress(memberAddress);
-//							
-//	return null;
-//	}
-	
-	
-	//在jsp裡接到Spring form表單裡 modelAttribute="" 接到的資料
 	@PostMapping("/member/post")
-	public String insertMember(@ModelAttribute Members member, 
-			@ModelAttribute MemberDetails memberDetail, Model m) {
-				memberService.insertMember(member);
-				memberDetailService.insertMemberDetails(memberDetail);
-				Members member2 = new Members();
-				MemberDetails memberDetail = new MemberDetails();
-				m.addAttribute("member", member2);
-				m.addAttribute("memberDetail", memberDetail);
-				
-				return null;
+	public String memberSignUp(@RequestParam("memberName") String memberName, 
+			@RequestParam("memberEmail") String memberAccount, 
+			@RequestParam("memberEmail") String memberEmail, 
+			@RequestParam("memberPassword") String memberPassword, 
+			@RequestParam("membervalid") Integer memberValid,
+			@RequestParam("memberLevel") Integer memberLevel,
+			@RequestParam("memberSex") Integer memberSex,
+			@RequestParam("memberBirthday") Date memberBirthday,
+			@RequestParam("memberPhone") String memberPhone, 
+			@RequestParam("memberPhone") String memberCellPhone,
+			@RequestParam("memberAddress") String memberAddress) {
+		Members member = new Members();
+		MemberDetails memberDetail = new MemberDetails();
+		memberDetail.setMemberName(memberName);
+		member.setMemberAccount(memberAccount);
+		memberDetail.setMemberEmail(memberEmail);
+		member.setMemberPassword(memberPassword);
+		member.setMemberValid(memberValid);
+		member.setMemberLevel(memberLevel);
+		memberDetail.setMemberSex(memberSex);
+		memberDetail.setMemberBirthday(memberBirthday);
+		memberDetail.setMemberPhone(memberPhone);
+		memberDetail.setMemberAddress(memberAddress);
+							
+	return null;
 	}
+	
+	
+//	//在jsp裡接到Spring form表單裡 modelAttribute="" 接到的資料
+//	@PostMapping("/member/post")
+//	public String insertMember(@ModelAttribute Members member, 
+//			@ModelAttribute MemberDetails memberDetail, Model m) {
+//				memberService.insertMember(member);
+//				memberDetailService.insertMemberDetails(memberDetail);
+//				Members member2 = new Members();
+//				MemberDetails memberDetail = new MemberDetails();
+//				m.addAttribute("member", member2);
+//				m.addAttribute("memberDetail", memberDetail);
+//				
+//				return null;
+//	}
 	
 }
