75255e7e6e36b03bddc53b516922965500628627
Harrycheng1992 <66951332+Harrycheng1992@users.noreply.github.com>
2023-02-16 09:50:32 +0800

    SOME

--- src/main/java/com/bookstrap/harry/controller/MemberAdmiController.java
+++ src/main/java/com/bookstrap/harry/controller/MemberAdmiController.java
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-//package com.bookstrap.harry.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//import com.bookstrap.harry.service.MemberDdetailService;
-//import com.bookstrap.harry.service.MemberService;
-//
-//@Controller
-//public class MemberAdmiController {
-//
-//	@Autowired
-//	private MemberService memberService; 
-//	
-//	@Autowired
-//	private MemberDdetailService memberDetailService;
-//	
-//	
-//	@GetMapping("/memberadmi/main")
-//	public String toAdmiMember() {
-//		return "member/Admi/template";
-//	}
-//	
-//}
-=======
+
 package com.bookstrap.harry.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,4 +24,4 @@ public class MemberAdmiController {
 	}
 	
 }
->>>>>>> parent of bc81646 (Dashboard add)
+
