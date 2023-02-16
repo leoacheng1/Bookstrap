5e5b00c35555b74ea82e140e2616ef097cb91497
Harrycheng1992 <66951332+Harrycheng1992@users.noreply.github.com>
2023-02-16 09:45:25 +0800

    SOME

--- src/main/java/com/bookstrap/harry/controller/MemberAdmiController.java
+++ src/main/java/com/bookstrap/harry/controller/MemberAdmiController.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //package com.bookstrap.harry.controller;
 //
 //import org.springframework.beans.factory.annotation.Autowired;
@@ -23,3 +24,30 @@
 //	}
 //	
 //}
+=======
+package com.bookstrap.harry.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bookstrap.harry.service.MemberDdetailService;
+import com.bookstrap.harry.service.MemberService;
+
+@Controller
+public class MemberAdmiController {
+
+	@Autowired
+	private MemberService memberService; 
+	
+	@Autowired
+	private MemberDdetailService memberDetailService;
+	
+	
+	@GetMapping("memberadmi/main")
+	public String toAdmiMember() {
+		return "member/Admi/MemberAdmiMain";
+	}
+	
+}
+>>>>>>> parent of bc81646 (Dashboard add)
--- src/main/java/com/bookstrap/harry/controller/MemberController.java
+++ src/main/java/com/bookstrap/harry/controller/MemberController.java
@@ -10,21 +10,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.bookstrap.harry.bean.MemberDetails;
 import com.bookstrap.harry.bean.Members;
 import com.bookstrap.harry.service.MemberDdetailService;
 import com.bookstrap.harry.service.MemberService;
+import com.bookstrap.harry.service.SendEmailService;
 
 @Controller
 //@SessionAttributes("memberDetail")
@@ -35,7 +41,8 @@ public class MemberController {
 	@Autowired
 	private MemberDdetailService memberDetailService;
 
-	
+	@Autowired
+	private SendEmailService emailService;
 	
 	@GetMapping("/member/signin")
 	public String memberSignIn(HttpSession session) {
