2127e53c01174ecc85ede1c7ccfc6a318c9232dd
Harrycheng1992 <66951332+Harrycheng1992@users.noreply.github.com>
2023-02-16 10:15:19 +0800

    update

--- src/main/java/com/bookstrap/harry/controller/MemberAdmiController.java
+++ src/main/java/com/bookstrap/harry/controller/MemberAdmiController.java
@@ -1,27 +1,27 @@
-
-package com.bookstrap.harry.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.bookstrap.harry.service.MemberDdetailService;
-import com.bookstrap.harry.service.MemberService;
-
-@Controller
-public class MemberAdmiController {
-
-	@Autowired
-	private MemberService memberService; 
-	
-	@Autowired
-	private MemberDdetailService memberDetailService;
-	
-	
-	@GetMapping("memberadmi/main")
-	public String toAdmiMember() {
-		return "member/Admi/MemberAdmiMain";
-	}
-	
-}
+//
+//package com.bookstrap.harry.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//import com.bookstrap.harry.service.MemberDdetailService;
+//import com.bookstrap.harry.service.MemberService;
+//
+//@Controller
+//public class MemberAdmiController {
+//
+//	@Autowired
+//	private MemberService memberService; 
+//	
+//	@Autowired
+//	private MemberDdetailService memberDetailService;
+//	
+//
+//	@GetMapping("memberadmi/main")
+//	public String toAdmiMember() {
+//		return "member/Admi/MemberAdmiMain";
+//	}
+//	
+//}
 
--- src/main/java/com/bookstrap/harry/dao/MemberRepository.java
+++ src/main/java/com/bookstrap/harry/dao/MemberRepository.java
@@ -1,9 +1,9 @@
 package com.bookstrap.harry.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.bookstrap.harry.bean.Members;
 
@@ -11,5 +11,17 @@ public interface MemberRepository extends JpaRepository<Members, Integer> {
 
 	@Query(value = "SELECT*FROM Members WHERE acccount = :mAccount", nativeQuery = true)
 	public Members findIdByEmail(@Param("mAccount") String memberEmail);
+	
+	
+	@Query("SELECT m FROM Members m WHERE m.vertificationCode = ?1")
+	public Members findByVerificationCode(String code);
+	
+	@Query(value = "SELECT*FROM Members WHERE vertification_code = :mCode", nativeQuery = true)
+	public Members findByVerifyCode(@Param("mCode") String code);
+	
+	@Modifying
+	@Query(value = "UPDATE Members SET valid = :mValid WHERE member_id = :mId", nativeQuery = true)
+	public Integer updateMemberValid(@Param("mValid") Integer valid, @Param("mId") Integer memberId);
+	
 	
 }
