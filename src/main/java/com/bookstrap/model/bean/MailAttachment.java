41490d3dc6fe701585f366c4f83da681e2ab31c1
Eddie <eddie5223@hotmail.com>
2023-02-20 07:51:07 +0800

    add beans

---
+++ src/main/java/com/bookstrap/model/bean/MailAttachment.java
@@ -0,0 +1,14 @@
+package com.bookstrap.model.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="MailAttachment")
+public class MailAttachment {
+
+	public MailAttachment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/bookstrap/model/bean/MailCategory.java
@@ -0,0 +1,14 @@
+package com.bookstrap.model.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="MailCategory")
+public class MailCategory {
+
+	public MailCategory() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
