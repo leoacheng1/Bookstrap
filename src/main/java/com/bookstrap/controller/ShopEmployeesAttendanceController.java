e92f2f57ae6b2dd82e4e0ca60c06f0f2c7ea1a6e
leoacheng <leoacheng@gmail.com>
2023-02-20 10:53:27 +0800

    reset version to 42d1c65

--- src/main/java/com/bookstrap/controller/ShopEmployeesAttendanceController.java
+++ src/main/java/com/bookstrap/controller/ShopEmployeesAttendanceController.java
@@ -1,47 +1,70 @@
 package com.bookstrap.controller;
 
-import java.util.List;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bookstrap.model.bean.ShopEmployeesAttendance;
 import com.bookstrap.service.ShopEmployeesAttendanceService;
 
+@Controller
 public class ShopEmployeesAttendanceController {
 
     @Autowired
     private ShopEmployeesAttendanceService sempattService;
+    
+    @GetMapping("/sempsatt/insertpage")
+	public String insertPage() {
+		return "shopemployees/insertsempsatt";
+	}
 
-    @GetMapping("")
-    public List<ShopEmployeesAttendance> getAllShopEmployeesAttendance() {
-        return sempattService.getAllShopEmployeesAttendance();
+
+    @PostMapping("sempsatt/insert")
+    public String addShopEmployeesAttendance(
+    		@RequestParam("attSempid") Integer attSempid, 
+    		@RequestParam("attDate") @DateTimeFormat(pattern="yyyy-MM-dd") Date attDate,
+			@RequestParam("attVacation") String attVacation, 
+			@RequestParam("attTime") Double attTime,
+			@RequestParam("attAgree") String attAgree) {
+    	
+    	try {
+			ShopEmployeesAttendance sempAtt = new ShopEmployeesAttendance();
+			sempAtt.setAttSempid(attSempid);
+			sempAtt.setAttDate(attDate);
+			sempAtt.setAttVacation(attVacation);
+			sempAtt.setAttTime(attTime);
+			sempAtt.setAttAgree(attAgree);
+			sempattService.addShopEmployeesAttendance(sempAtt);
+			return "shopemployees/home";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return "shopemployees/insertsempsatt";
     }
 
-    @GetMapping("/{attId}")
-    public ShopEmployeesAttendance getShopEmployeesAttendanceById(@PathVariable Integer attId) {
-        return sempattService.getShopEmployeesAttendanceById(attId);
-    }
-
-    @PostMapping("")
-    public void addShopEmployeesAttendance(@RequestBody ShopEmployeesAttendance shopEmployeesAttendance) {
-    	sempattService.addShopEmployeesAttendance(shopEmployeesAttendance);
-    }
-
-    @PutMapping("/{attId}")
-    public void updateShopEmployeesAttendance(@RequestBody ShopEmployeesAttendance shopEmployeesAttendance, @PathVariable Integer attId) {
-        shopEmployeesAttendance.setAttId(attId);
-        sempattService.updateShopEmployeesAttendance(shopEmployeesAttendance);
-    }
-
-    @DeleteMapping("/{attId}")
-    public void deleteShopEmployeesAttendance(@PathVariable Integer attId) {
-    	sempattService.deleteShopEmployeesAttendance(attId);
-    }
+//    @GetMapping("")
+//    public List<ShopEmployeesAttendance> getAllShopEmployeesAttendance() {
+//    	return sempattService.getAllShopEmployeesAttendance();
+//    }
+//    
+//    @GetMapping("/{attId}")
+//    public ShopEmployeesAttendance getShopEmployeesAttendanceById(@PathVariable Integer attId) {
+//    	return sempattService.getShopEmployeesAttendanceById(attId);
+//    }
+//    @PutMapping("/{attId}")
+//    public void updateShopEmployeesAttendance(@RequestBody ShopEmployeesAttendance shopEmployeesAttendance, @PathVariable Integer attId) {
+//        shopEmployeesAttendance.setAttId(attId);
+//        sempattService.updateShopEmployeesAttendance(shopEmployeesAttendance);
+//    }
+//
+//    @DeleteMapping("/{attId}")
+//    public void deleteShopEmployeesAttendance(@PathVariable Integer attId) {
+//    	sempattService.deleteShopEmployeesAttendance(attId);
+//    }
 
 }
--- src/main/java/com/bookstrap/service/ShopEmployeesAttendanceService.java
+++ src/main/java/com/bookstrap/service/ShopEmployeesAttendanceService.java
@@ -6,32 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookstrap.model.ShopEmployeesAttendanceRepository;
+import com.bookstrap.model.ShopEmployeesRepository;
+import com.bookstrap.model.Shops;
+import com.bookstrap.model.bean.ShopEmployees;
 import com.bookstrap.model.bean.ShopEmployeesAttendance;
 
 @Service
 public class ShopEmployeesAttendanceService {
 
-    @Autowired
-    private ShopEmployeesAttendanceRepository sempattDao;
+	@Autowired
+	private ShopEmployeesRepository sempDao;
 
-    public List<ShopEmployeesAttendance> getAllShopEmployeesAttendance() {
-        return sempattDao.findAll();
-    }
+	@Autowired
+	private ShopEmployeesAttendanceRepository sempattDao;
 
-    public ShopEmployeesAttendance getShopEmployeesAttendanceById(Integer attId) {
-        return sempattDao.findById(attId).orElse(null);
-    }
+	public List<ShopEmployeesAttendance> getAllShopEmployeesAttendance() {
+		return sempattDao.findAll();
+	}
 
-    public void addShopEmployeesAttendance(ShopEmployeesAttendance shopEmployeesAttendance) {
-    	sempattDao.save(shopEmployeesAttendance);
-    }
+	public ShopEmployeesAttendance getShopEmployeesAttendanceById(Integer attId) {
+		return sempattDao.findById(attId).orElse(null);
+	}
 
-    public void updateShopEmployeesAttendance(ShopEmployeesAttendance shopEmployeesAttendance) {
-    	sempattDao.save(shopEmployeesAttendance);
-    }
+	public ShopEmployeesAttendance addShopEmployeesAttendance(ShopEmployeesAttendance shopEmployeesAttendance) {
+		Integer sempattId = shopEmployeesAttendance.getAttSempid();
+		ShopEmployees semps = sempDao.findById(sempattId).get();
+		shopEmployeesAttendance.setAttSemps(semps);
+		return sempattDao.save(shopEmployeesAttendance);
+	}
 
-    public void deleteShopEmployeesAttendance(Integer attId) {
-    	sempattDao.deleteById(attId);
-    }
+	public void updateShopEmployeesAttendance(ShopEmployeesAttendance shopEmployeesAttendance) {
+		sempattDao.save(shopEmployeesAttendance);
+	}
+
+	public void deleteShopEmployeesAttendance(Integer attId) {
+		sempattDao.deleteById(attId);
+	}
 
 }
