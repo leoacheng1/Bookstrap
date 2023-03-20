f50ca48a5df29bd634c67b1b59e59b24bcd4c70a
Eddie <eddie@hotmail.com>
2023-03-20 09:29:40 +0800

    identity

--- src/main/java/com/bookstrap/model/bean/MailAccount.java
+++ src/main/java/com/bookstrap/model/bean/MailAccount.java
@@ -24,7 +24,7 @@ public class MailAccount {
 	
 	@Id
 	@Column(name = "account_id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer accountId;
 
 	@Column(name = "account")
